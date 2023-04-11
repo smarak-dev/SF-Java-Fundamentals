38d5f8da0dcbcab08cf1ca42f5fb71dde4ce3d09
simon <simon@dancingcloudservices.com>
2023-04-11 17:31:20 -0600

    Day one 16:30 (lab)

--- src/main/java/doingio/Example.java
+++ src/main/java/doingio/Example.java
@@ -3,9 +3,20 @@ package doingio;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Example {
   public static void main(String[] args) throws IOException {
+    /*
+    Modify:
+    prompt the user for a filename
+    if the file exists, print its contents
+    if not... go back for another filename
+     */
+    BufferedReader keyboard = new BufferedReader(
+        new InputStreamReader(System.in));
+    // can now read lines from the keyboard
+
     FileReader fr = new FileReader("data.dat");
     BufferedReader input = new BufferedReader(fr);
     String line;
